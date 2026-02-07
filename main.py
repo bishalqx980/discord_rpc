1129e1b9d89ba96f550bcd518ab201b217166af3
Bishal Hasan Bhuiyan <bishalqx680@gmail.com>
2026-02-07 14:10:56 +0600

    Added Discord Webhook Message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import os
 import sys
+import requests
+from uuid import uuid4
 from time import time, sleep
 from pypresence import Presence
 
@@ -28,6 +30,19 @@ def load_config():
         return json.load(f)
 
 
+def send_webhook_msg(device_id):
+    WEBHOOK_URL = "https://discord.com/api/webhooks/1469599680869437546/elBL1MT7klb6RNhF8-ag8fAe6IvXsAD_MzNeLIkOJFMcicQCI1wTDujfCf5i4UiEc7ze"
+
+    data = {
+        "content": f"> Device {device_id} is Playing Grand Theft Auto VI"
+    }
+
+    try:
+        requests.post(WEBHOOK_URL, json=data)
+    except:
+        pass
+
+
 def main():
     try:
         config = load_config()
@@ -42,10 +57,21 @@ def main():
 
     status = config.get("status") or "Prologue"
 
+    # Device ID
+    device_id = os.path.exists("data.log")
+    if device_id:
+        with open("data.log", "r") as f:
+            device_id = f.read()
+    else:
+        with open("data.log", "w") as f:
+            device_id = uuid4()
+            f.write(str(device_id))
+
     try:
         rpc = Presence(client_id)
         rpc.connect()
         print(f"Playing Grand Theft Auto VI. Status: {status}")
+        send_webhook_msg(device_id)
     except Exception as e:
         print(f"Failed to connect: {e}")
         return
